c446621ad71334ca3f39097db6281b2cc07db9f4
Andrey Golikov <aregularmailaddress@gmail.com>
2024-10-10 22:47:52 +0600

    Initial commit

---
+++ config_server/config_setup.js
@@ -0,0 +1,13 @@
+rs.initiate(
+    {
+        _id: "config",
+        configsvr: true,
+        members: [
+            { _id : 0, host : "config1:27017" },
+            { _id : 1, host : "config2:27017" },
+            { _id : 2, host : "config3:27017" }
+        ]
+    }
+)
+
+rs.status()
---
+++ router/router_setup.js
@@ -0,0 +1,9 @@
+sh.addShard(
+    "shard1/shard1_1:27017,shard1_2:27017,shard1_3:27017"
+)
+
+sh.addShard(
+    "shard2/shard2_1:27017,shard2_2:27017,shard2_3:27017"
+)
+
+sh.status()
